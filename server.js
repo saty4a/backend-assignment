2b8dde7b07323bf59328f58ee28f651e57bfe4f5
saty4a <satyabratbiswal5413@gmail.com>
2024-01-09 04:48:41 +0530

    first commit

--- server.js
+++ server.js
@@ -28,5 +28,5 @@ app.use((error, req, res, next) => {
 });
 
 app.listen(5000, () => {
-  console.log("It's running at http://127.0.0.1:5000");
+  console.log("It's running at http://localhost:5000");
 });
